994bf998cfd3115a111557fbb9418d6f8fbca134
kevinezarizky <kezar08@gmail.com>
2018-12-01 11:21:15 +0700

    READ

--- read.py
+++ read.py
@@ -1,2 +1,12 @@
+import json
+
 print('Read JSON file')
+
+def getJSON(filepath):
+    with open(filepath, 'r') as fp:
+        return json.load(fp)
+
+myObj = getJSON('./latih.json')
+
+print(myObj.get("mahasiswa"))
 
